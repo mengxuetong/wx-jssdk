3f3ce3f28ee7e5e160d976c457346a04ff540f9a
mengxuetong <mengxuetong@foxmail.com>
2015-11-03 12:15:55 +0800

    allow origin:

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var users = require('./routes/jssdk');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/jssdk.js
+++ routes/jssdk.js
@@ -3,15 +3,7 @@ var express = require('express');
 var jsSHA = require('jssha');
 var router = express.Router();		
 var ticket,noncestr,timestamp,url;
-//设置跨域访问
-router.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+
 router.get('/jssdk',function(req,res){
 
 	http.get('http://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxdd42174ce7038c28&secret=d4624c36b6795d1d99dcf0547af5443d', function(_res) {
